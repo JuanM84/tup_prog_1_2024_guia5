e8efb9a5b466c2c36513746759b87901adb2f505
JuanM <jmgonzalez.parana@gmail.com>
2024-05-17 18:11:38 -0300

    ejercicio 2

--- Guia5/Ejercicio1/Program.cs
+++ Guia5/Ejercicio1/Program.cs
@@ -10,6 +10,46 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            string nombre, nombreMenor="", nombreMayor="", jug;
+            float promedio, menor=0, mayor=0;
+            
+            Console.WriteLine("Ingrese el Nombre del Jugador: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el Promedio de {0}:", nombre);
+            promedio = Convert.ToSingle(Console.ReadLine());
+            menor = promedio;
+            nombreMenor = nombre;
+            mayor = promedio;
+            nombreMayor = nombre;
+            Console.WriteLine("Desea ingresar otro jugador? (S/N)");
+            jug = Console.ReadLine();
+            while (jug == "S" || jug == "s")
+            {
+                Console.WriteLine("Ingrese el Nombre del Jugador: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el Promedio de {0}:", nombre);
+                promedio = Convert.ToSingle(Console.ReadLine());
+                //Es menor promedio?
+                if (promedio < menor)
+                {
+                    menor = promedio;
+                    nombreMenor = nombre;
+                }
+                //Es mayor promedio?
+                if (promedio > mayor)
+                {
+                    mayor = promedio;
+                    nombreMayor = nombre;
+                }
+                Console.WriteLine("Desea ingresar otro jugador? (S/N)");
+                jug = Console.ReadLine();
+
+            }
+            
+            Console.WriteLine("---------------");
+            Console.WriteLine("El Jugador con mayor promedio es {0} con ${1}", nombreMayor, mayor);
+            Console.WriteLine("El Jugador con menor promedio es {0} con ${1}", nombreMenor, menor);
+            Console.ReadKey();
         }
     }
 }
--- Guia5/Ejercicio2/Program.cs
+++ Guia5/Ejercicio2/Program.cs
@@ -10,10 +10,35 @@ namespace Ejercicio2
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Magenta;
             Console.Clear();
 
-            Console.WriteLine("hola mundo!");
+            int anterior = 0, anterior_anterior = 0, actual=0, hasta;
+
+            Console.WriteLine("Ingresar cuantos números desea mostrar la serie de Fibonacci: ");
+            hasta = Convert.ToInt32(Console.ReadLine());
+
+
+            for(int num=0; num <= hasta; num++)   
+            {
+                //if( num == 0 ) actual = 0;
+                //if (num == 1) actual = 1;
+                anterior_anterior = anterior;
+                anterior = actual;
+                switch (num)
+                {
+                    case 0:
+                        actual = 0;
+                        break;
+                    case 1:
+                        actual = 1; 
+                        break;
+                    default:
+                        actual = anterior + anterior_anterior; 
+                        break;
+                }
+                Console.WriteLine(actual);
+            }
 
             Console.ReadKey();
         }
